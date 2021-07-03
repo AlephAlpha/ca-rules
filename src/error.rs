2c871fe252f769eab51cb70c4c569fb78b7e53a7
AlephAlpha <alephalpha911@gmail.com>
2021-07-03 13:06:15 +0800

    :bookmark: v0.3.3

--- src/error.rs
+++ src/error.rs
@@ -1,27 +1,28 @@
 //! Errors that can be returned when parsing rule strings.
 
 // use std::fmt::{self, Display, Formatter};
+use displaydoc::Display;
 use thiserror::Error;
 
 /// Errors that can be returned when parsing rule strings.
-#[derive(Clone, Debug, PartialEq, Eq, Error)]
+#[derive(Clone, Debug, PartialEq, Eq, Error, Display)]
 pub enum ParseRuleError {
-    #[error("Missing expected {0:?}")]
+    /// Missing expected {0:?}
     Missing(char),
-    #[error("Missing expected number")]
+    /// Missing expected number
     MissingNumber,
-    #[error("Unexpected {0:?}")]
+    /// Unexpected {0:?}
     Unexpected(char),
-    #[error("Extra unparsed junk at the end of the rule string")]
+    /// Extra unparsed junk at the end of the rule string
     ExtraJunk,
-    #[error("Number of states less than 2 in Generations rule")]
+    /// Number of states less than 2 in Generations rule
     GenLessThan2,
-    #[error("Not a MAP rule")]
+    /// Not a MAP rule
     NotMapRule,
-    #[error("Invalid Base64 encoding for MAP rule")]
+    /// Invalid Base64 encoding for MAP rule
     Base64Error,
-    #[error("Invalid length for MAP rule")]
+    /// Invalid length for MAP rule
     InvalidLength,
-    #[error("Generations number overflow for Generations rule")]
+    /// Generations number overflow for Generations rule
     GenOverflow,
 }
